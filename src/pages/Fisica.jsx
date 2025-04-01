4c7edd03140dc617e59ffa9fb9ebd7b4546f95f3
Diana <diana.sanchez.o@uniautonoma.edu.co>
2025-04-01 17:06:47 -0500

    DiseñoPagesCategory

--- src/pages/Fisica.jsx
+++ src/pages/Fisica.jsx
@@ -1,30 +1,27 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import CategoriaProyectos from "../components/CategoriaProyectos";
 
-const projects = [
-  { title: "Simulación 1", description: "Descripción breve de la simulación 1.", image: "/img/sim1.png" },
-  { title: "Simulación 2", description: "Descripción breve de la simulación 2.", image: "/img/sim2.png" },
-  { title: "Simulación 3", description: "Descripción breve de la simulación 3.", image: "/img/sim3.png" },
-];
+const simulaciones = {
+    Fisica: ["Simulación A", "Simulación B", "Simulación C"]
+};
+
+const aplicaciones = {
+    Fisica: ["App 1", "App 2"]
+};
 
 const Fisica = () => {
-  return (
-    <div>
-      <Navbar />
-      <div className="page-content">
-        <h2>Simulaciones de Física</h2>
-        <div className="project-list">
-          {projects.map((project, index) => (
-            <div className="project-card" key={index}>
-              <img src={project.image} alt={project.title} />
-              <h3>{project.title}</h3>
-              <p>{project.description}</p>
-            </div>
-          ))}
+    return (
+        <div>
+            <Navbar />
+            {/* Sección de Simulaciones */}
+            <CategoriaProyectos titulo="Simulaciones de Física" categoria="Fisica" proyectos={simulaciones} />
+
+            {/* Sección de Aplicaciones Móviles */}
+            <CategoriaProyectos titulo="Aplicaciones Móviles de Física" categoria="Fisica" proyectos={aplicaciones} />
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Fisica;
+
