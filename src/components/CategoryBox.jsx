21a5c0022914d1287eb81d79edebe961bed7ce39
Diana <diana.sanchez.o@uniautonoma.edu.co>
2025-04-13 00:01:42 -0500

    modtailwindHome

--- src/components/CategoryBox.jsx
+++ src/components/CategoryBox.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const CategoryBox = ({ title, image, path }) => {
   return (
-    <Link to={path} className="category"> {/* Se mantiene la clase 'category' */}
-      <img src={image} alt={title} />
-      <p>{title}</p>
-    </Link>
+    <div className="category w-80 h-80 bg-white shadow-lg rounded-lg flex flex-col items-center justify-center cursor-pointer transition-transform transform hover:scale-105 hover:bg-teal-100 duration-200">
+      <Link to={path} className="flex flex-col items-center">
+        <img src={image} alt={title} className="w-24 h-auto object-contain mb-2" />
+        <span className="font-bold text-center">{title}</span>
+      </Link>
+    </div>
   );
 };
 
