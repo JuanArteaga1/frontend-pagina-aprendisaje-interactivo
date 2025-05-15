c3d15aba376e4994cf893ee4b91a2b6634a07b52
Diana <diana.sanchez.o@uniautonoma.edu.co>
2025-05-15 15:22:14 -0500

    editar-investigacion

--- src/main.jsx
+++ src/main.jsx
@@ -41,6 +41,7 @@ import ProtectedRote from "./ProtectedRoute";
 
 import EditarProyecto from "./pages/EditarProyecto";
 import EditarPodcast from "./pages/EditarPodcast";
+import EditarInvestigacion from "./pages/EditarInvestigacion";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <LoginProvider>
@@ -87,6 +88,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         //Ruta para editar proyecto
         <Route path="/editar-proyecto/:id" element={<ProyectosProvider><EditarProyecto /></ProyectosProvider>} />
         <Route path="/editar-podcast/:id" element={<PodcastProvider><EditarPodcast /></PodcastProvider>} />
+        <Route path="/editar-investigacion/:id" element={<InvestigacionProvider><EditarInvestigacion /></InvestigacionProvider>} />
 
         </Route>
 
