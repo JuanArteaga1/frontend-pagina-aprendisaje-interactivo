c5eb9440a30008055716405cc20160cc066b786f
tostako <juan.arteaga.f@uniautonoma.edu.co>
2025-08-21 18:00:51 -0500

    correciones

--- src/api/AdmiRegistro.js
+++ src/api/AdmiRegistro.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const Api = 'http://localhost:3000'
+const Api = import.meta.env.VITE_RUTA1
 
 export const RegistroDocenteAPI = Usuario => axios.post(`${Api}/Usuarios/registro`, Usuario)
 export const ValidarTokenAPI = (token) => axios.get(`${Api}/Usuarios/validar-token/${token}`)
