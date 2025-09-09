7db78df75a17f6eee125a0d0d57803ae629c05bf
Kevin22R <kevinjhoan685@gmail.com>
2025-09-08 20:18:55 -0500

    Kevin/Camb

--- src/components/CategoryBox.jsx
+++ src/components/CategoryBox.jsx
@@ -5,7 +5,10 @@ import { Link } from 'react-router-dom';
 const CategoryBox = ({ title, image, path }) => {
   return (
     <div className="category w-80 h-80 bg-white shadow-lg rounded-lg flex flex-col items-center justify-center cursor-pointer transition-transform transform hover:scale-105 hover:bg-teal-100 duration-200">
-      <Link to={path} className="flex flex-col items-center">
+      <Link
+        to={path}
+        className="category w-80 h-80 bg-white shadow-lg rounded-lg flex flex-col items-center justify-center cursor-pointer transition-transform transform hover:scale-105 hover:bg-teal-100 duration-200"
+      >
         <img src={image} alt={title} className="w-24 h-auto object-contain mb-2" />
         <span className="font-bold text-center">{title}</span>
       </Link>
